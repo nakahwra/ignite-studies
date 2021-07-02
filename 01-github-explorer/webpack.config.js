1d837337817c3a24291b1ced1fdc815b2d7081e9
Lucas Nakahara <nakahwra@gmail.com>
2021-07-02 17:34:20 -0300

    ✨ First component & props

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      inject: 'body'
     }),
   ],
   module: {
