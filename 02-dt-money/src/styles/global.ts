b1f776274c2f50781316a379b0a57544e7d39959
Lucas Nakahara <nakahwra@gmail.com>
2021-07-09 00:06:39 -0300

    💄 Create and stylize Dashboard & Summary components

--- 02-dt-money/src/styles/global.ts
+++ 02-dt-money/src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
 
     --blue-light: #6933ff;
 
@@ -33,7 +34,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background: var(---background);
-    /* -webkit-font-smoothing: antialiased; */
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
