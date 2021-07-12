70ccd2c19f572d2a2e18ef5521a3a0d9048d3107
Lucas Nakahara <nakahwra@gmail.com>
2021-07-12 16:21:17 -0300

    💄 Stylize new transaction modal

--- 02-dt-money/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saidas.svg";
+import closeImg from "../../assets/close.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
 Modal.setAppElement('#root');
 
@@ -16,11 +19,33 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
