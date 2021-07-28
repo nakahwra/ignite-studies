cb016a10b6ba1a7be63932994cfd2937f864f7f8
Lucas Nakahara <nakahwra@gmail.com>
2021-07-27 22:09:09 -0300

    💄 Set up dynamic title for index page

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.css';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello ig.news!
-    </h1>
+    <>
+      <Head>
+        <title>Início - ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello ig.news!
+      </h1>
+    </>
   )
 }
