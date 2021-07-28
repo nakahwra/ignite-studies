b5288ffde315e3915736f2128048ae1c768de212
Lucas Nakahara <nakahwra@gmail.com>
2021-07-27 22:34:45 -0300

    💄 Set up global style file

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.css';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início - ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello ig.news!
       </h1>
     </>
