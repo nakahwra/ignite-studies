d636ff11013f49d391daa53e5969ecca1852d089
Lucas Nakahara <nakahwra@gmail.com>
2021-07-30 11:14:41 -0300

    💄 Create and stylize home page

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,14 +1,26 @@
+import styles from './home.module.scss';
+
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início - ig.news</title>
+        <title>Home - ig.news</title>
       </Head>
-      <h1>
-        Hello ig.news!
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get acces to all the publication <br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
