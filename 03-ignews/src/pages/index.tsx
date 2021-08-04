8aba61b0a263b296dab5f5c3165fd5eb8d5bbdc9
Lucas Nakahara <nakahwra@gmail.com>
2021-08-03 23:24:56 -0300

    ♻️ Change sub. price to static gen. render method

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from './home.module.scss';
 
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JKXzgDem1LYkR2oDWPgpVtm');
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 25, //24 hours
   }
 }
