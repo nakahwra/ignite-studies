827015fb2e8af7edf8ee3409ae827ebc8fc49723
Lucas Nakahara <nakahwra@gmail.com>
2021-08-05 13:38:24 -0300

    ✨ Save signed in users to db

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+import { fauna } from '../../../services/fauna';
+import { query as q } from "faunadb";
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,4 +12,22 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 });
