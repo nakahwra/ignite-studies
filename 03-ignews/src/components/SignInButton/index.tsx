c9129931dabb562c49435352f0c8fa342d5d747d
Lucas Nakahara <nakahwra@gmail.com>
2021-08-05 00:44:15 -0300

    ✨ Add sign in with Github

--- 03-ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -2,23 +2,26 @@ import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d561"/>
-      Lucas Nakahara
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
