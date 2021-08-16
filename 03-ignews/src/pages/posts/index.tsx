3e9d21b944a70918a62b967cb60b01b5d0a1e2b6
Lucas Nakahara <nakahwra@gmail.com>
2021-08-16 11:19:40 -0300

    💄 List fetched posts in posts page

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -7,7 +7,18 @@ import { RichText } from 'prismic-dom';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -16,36 +27,15 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post: de fato, é o título.</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              In quis mi in orci tempus pulvinar.
-              Vestibulum ante ipsum primis in faucibus orci luctus et 
-              ultrices posuere cubilia curae
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post: de fato, é o título.</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              In quis mi in orci tempus pulvinar.
-              Vestibulum ante ipsum primis in faucibus orci luctus et 
-              ultrices posuere cubilia curae
-            </p>
-          </a>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post: de fato, é o título.</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              In quis mi in orci tempus pulvinar.
-              Vestibulum ante ipsum primis in faucibus orci luctus et 
-              ultrices posuere cubilia curae
-            </p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          )) }
         </div>
       </main>
     </>
