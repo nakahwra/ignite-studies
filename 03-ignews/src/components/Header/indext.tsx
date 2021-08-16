77d21366f09a50e1fc3f7668094ade2cba6fb93c
Lucas Nakahara <nakahwra@gmail.com>
2021-08-16 11:55:38 -0300

    ✨ Set up pages linking

--- 03-ignews/src/components/Header/indext.tsx
+++ 03-ignews/src/components/Header/indext.tsx
@@ -1,14 +1,28 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
+import { ActiveLink } from '../ActiveLink';
+
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <img src="/images/logo.svg" alt="ig.news" />
+        </Link>
+
         <nav>
-          <a className={styles.active} href="#">Home</a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
