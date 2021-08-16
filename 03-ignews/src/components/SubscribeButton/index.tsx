a0be16499228527feb49e9fe0660ea83af5333d1
Lucas Nakahara <nakahwra@gmail.com>
2021-08-16 14:21:29 -0300

    ✨ Redirect subbed users when sub button is clicked

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -9,10 +11,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
