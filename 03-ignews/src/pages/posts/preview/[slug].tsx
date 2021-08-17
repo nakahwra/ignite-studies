b4d2d576e0026471a5ec6bec1c96bf701e75a9a7
Lucas Nakahara <nakahwra@gmail.com>
2021-08-17 15:26:05 -0300

    💄 Create and stylize post preview

---
+++ 03-ignews/src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,88 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+
+import { RichText } from "prismic-dom";
+import { getPrismiscClient } from "../../../services/prismic";
+
+import styles from '../post.module.scss';
+import Link from "next/link";
+import { useSession } from "next-auth/client";
+import { useEffect } from "react";
+import router from "next/router";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session])
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} - Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{__html: post.content}}
+          />
+
+          <Link href="/">
+            <div className={styles.continueReading}>
+              Wanna continue reading?
+                <a>Subscribe now</a> 🤗
+              
+            </div>
+          </Link>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismiscClient();
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
