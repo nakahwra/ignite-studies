e58c342efa8c6dff789b0da1863623da8660e292
Lucas Nakahara <nakahwra@gmail.com>
2021-12-23 02:04:24 -0300

    💄 create input component

---
+++ 04-dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+
+// Estende os props padrão do Input
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+const Input = ({ name, label, ...rest }: InputProps) => {
+  return (
+    <FormControl>
+      { label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+
+      <ChakraInput
+        id={name}
+        name={name}
+        type="email"
+        focusBorderColor='pink.500'
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bg: "gray.900"
+        }}
+        size="lg"
+        {...rest} 
+        // Habilita o componente a receber todos os props padrão do ChakraInput
+      />
+    </FormControl>
+  )
+}
+
+export default Input;
