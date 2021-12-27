b54351d4dae705531b615b942fceb1282f54745d
Lucas Nakahara <nakahwra@gmail.com>
2021-12-27 17:06:55 -0300

    💄 Add Sidebar to Dashboard page

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
-import { Header } from "../components";
+import { Flex } from "@chakra-ui/react";
+import { Header, Sidebar } from "../components";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+        <h1>Content</h1>
+      </Flex>
+    </Flex>
   )
 }
