576d8fed89e2df3bf0cb51951230e5b233f593ee
Lucas Nakahara <nakahwra@gmail.com>
2022-01-07 09:24:03 -0300

    📱 Fix Users pages responsiveness

--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -10,24 +10,24 @@ export default function CreateUser() {
       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
           
           <Stack spacing="8">
-            <SimpleGrid minChildWidth="240px" w="100%" spacing="8">
+            <SimpleGrid minChildWidth="240px" w="100%" spacing={["6", "8"]}>
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
             
-            <SimpleGrid minChildWidth="240px" w="100%" spacing="8">
+            <SimpleGrid minChildWidth="240px" w="100%" spacing={["6", "8"]}>
               <Input name="password" type="password" label="Senha" />
               <Input name="password_confirm" type="password" label="Confirmar senha" />
             </SimpleGrid>
           </Stack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt="8" justify={["center", "flex-end"]}>
             <Stack direction="row" spacing="4">
               <Button colorScheme="whiteAlpha">Cancelar</Button>
               <Button colorScheme="pink">Salvar</Button>
--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -1,8 +1,13 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header, Pagination, Sidebar } from "../../components";
 
 export default function UserList() {
+  const isWidescreen = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -29,17 +34,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th width="8" px="6" color="gray.300">
+                <Th width="8" px={["4", "4", "6"]} color="gray.300">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                { isWidescreen && <Th>Data de cadastro</Th> }
                 <Th w="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -48,21 +53,23 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">nakahara@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>24 de Dezembro, 2021</Td>
+                { isWidescreen && <Td>24 de Dezembro, 2021</Td> }
                 <Td>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
-                  >
-                    Editar
-                  </Button>
+                  { isWidescreen && (
+                    <Button 
+                      as="a" 
+                      size="sm" 
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
+                    >
+                      Editar
+                    </Button>
+                  )}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -71,21 +78,23 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">nakahara@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>24 de Dezembro, 2021</Td>
+                { isWidescreen && <Td>24 de Dezembro, 2021</Td> }
                 <Td>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
-                  >
-                    Editar
-                  </Button>
+                  { isWidescreen && (
+                    <Button 
+                      as="a" 
+                      size="sm" 
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
+                    >
+                      Editar
+                    </Button>
+                  )}
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -94,17 +103,19 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">nakahara@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>24 de Dezembro, 2021</Td>
+                { isWidescreen && <Td>24 de Dezembro, 2021</Td> }
                 <Td>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
-                  >
-                    Editar
-                  </Button>
+                  { isWidescreen && (
+                    <Button 
+                      as="a" 
+                      size="sm" 
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize={16}/>}
+                    >
+                      Editar
+                    </Button>
+                  )}
                 </Td>
               </Tr>
             </Tbody>
