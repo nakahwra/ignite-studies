1639236063a014c17ebf15d67d246fea585ca967
Lucas Nakahara <nakahwra@gmail.com>
2022-01-07 17:57:50 -0300

    ✨ Implement input validation in user creation form

--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -1,9 +1,41 @@
 import Link from "next/link";
 import { Box, Button, Divider, Flex, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from 'yup';
+
 import { Header, Sidebar } from "../../components";
 import { Input } from "../../components/Form";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'Necessário no mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    null,
+    yup.ref('password')
+  ], 'As senhas precisam ser iguais'),
+})
+
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema)
+  })
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = (data) => {
+    console.log(data);
+  }
+
   return (
     <Box>
       <Header />
@@ -11,29 +43,71 @@ export default function CreateUser() {
       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
-          
+
           <Stack spacing="8">
             <SimpleGrid minChildWidth="240px" w="100%" spacing={["6", "8"]}>
-              <Input name="name" label="Nome completo" />
-              <Input name="email" type="email" label="E-mail" />
+              <Input
+                name="name"
+                type="text"
+                label="Nome completo"
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register('email')}
+              />
             </SimpleGrid>
-            
+
             <SimpleGrid minChildWidth="240px" w="100%" spacing={["6", "8"]}>
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirm" type="password" label="Confirmar senha" />
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register('password')}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmar senha"
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
+              />
             </SimpleGrid>
           </Stack>
 
           <Flex mt="8" justify={["center", "flex-end"]}>
             <Stack direction="row" spacing="4">
               <Link href="/users" passHref>
-                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+                <Button
+                  as="a"
+                  colorScheme="whiteAlpha"
+                >
+                  Cancelar
+                </Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
             </Stack>
           </Flex>
         </Box>
